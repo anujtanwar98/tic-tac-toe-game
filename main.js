aaed8902522a490ecfac3bbc3fbbe42025c60274
Anuj <at3459@drexel.edu>
2022-02-20 20:57:50 -0500

    :bug: :lipstick:

--- main.js
+++ main.js
@@ -17,13 +17,13 @@ const checkWin = ()=> {
         [1, 4, 7, 5, 15, 90],
         [2, 5, 8, 15, 15, 90],
         [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 45],
+        [2, 4, 6, 5, 15, -45],
     ]
     wins.forEach(e =>{
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
             document.querySelector('.sub-sub-info').innerText = boxtext[e[0]].innerText + " Won"
             gameover = true;
-            document.querySelector('.thank-you-image').getElementsByTagName('img')[0].style.width= "200px"
+            document.querySelector('.thank-you-image').getElementsByTagName('img')[0].style.width= "300px"
             document.querySelector(".line").style.width = "20vw";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
         }
